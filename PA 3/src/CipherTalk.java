26c055367e72a02e96b083c855c133487ef96600
Rob Johansen <robert_johans22@hotmail.com>
2014-04-23 23:09:53 -0600

    Commit code for generating the message digest and signing it.

--- PA 3/src/CipherTalk.java
+++ PA 3/src/CipherTalk.java
@@ -1,25 +1,42 @@
 import java.io.*;
 import java.security.*;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.*;
+
+import javax.crypto.*;
 
 public class CipherTalk {
 	
 	/* Command-Line Options */
 	private static String  address;
-	private static String  message;
+	private static int     port = -1;
+	private static String  messageString;
+	private static byte[]  message;
 	private static Boolean verbose = false;
 	
 	/* Keys */
 	private static final String ALICE_PRIVATE_KEY_FILENAME = "aliceprivate.der";
 	private static PrivateKey alicePrivateKey;
+	private static byte[] symmetricKey;
 	
-	public static void main(String[] args) throws FileNotFoundException, IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+	/* Message Hash */
+	private static byte[] messageHash;
+	
+	/* Signature */
+	private static byte[] signature;
+	
+	public static void main(String[] args) throws FileNotFoundException,
+												  IOException,
+												  NoSuchAlgorithmException,
+												  InvalidKeySpecException,
+												  InvalidKeyException,
+												  SignatureException {
 		for (int i = 0; i < args.length; i++) {
 			if (args[i].equals("-a") && args.length >= i)
 				address = args[i+1];
+			if (args[i].equals("-p") && args.length >= i)
+				try { port = Integer.parseInt(args[i+1]); } catch (NumberFormatException e) { error("Invalid port"); }
 			if (args[i].equals("-m") && args.length >= i)
-				message = args[i+1];
+				messageString = args[i+1];
 			if (args[i].equals("-v"))
 				verbose = true;
 			if (args[i].equals("-h"))
@@ -29,13 +46,36 @@ public class CipherTalk {
 		if (address == null)
 			error("Missing argument: -a <address>");
 		
-		if (message == null)
+		if (port == -1)
+			error("Missing argument: -p <port>");
+		
+		if (messageString == null || messageString.length() == 0)
 			error("Missing argument: -m <message>");
 		
+		message = messageString.getBytes();
+		
+		vout("\n========================================\n"
+		     + "    Starting CipherTalk Transmission\n"
+		     + "========================================\n");
+		
 		signMessage();
+		encryptMessage();
+		
+		vout(""); // If output is verbose, add a new line at the very end
 	}
 	
-	private static void signMessage() throws FileNotFoundException, IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+	private static void signMessage() throws FileNotFoundException,
+											 IOException,
+											 NoSuchAlgorithmException,
+											 InvalidKeySpecException,
+											 InvalidKeyException,
+											 SignatureException {
+		vout("Creating a hash of the message");
+		
+		MessageDigest md = MessageDigest.getInstance("SHA-1");
+		md.update(message);
+		messageHash = md.digest();
+		
 		File f = new File(ALICE_PRIVATE_KEY_FILENAME);
 		
 		if (!f.exists())
@@ -54,31 +94,51 @@ public class CipherTalk {
 		if (alicePrivateKey == null)
 			error("Problem with Alice's private key...");
 		
-		// WYLO: Generate a hash of the message and sign it with Alice's private key...
+		vout("Signing the hash using Alice's private key");
 		
-		message = "-----BEGIN CipherTalk SIGNED MESSAGE-----\n" + message + "\n";
-		
-//		String signature
+		Signature rsa = Signature.getInstance("SHA1withRSA");
+		rsa.initSign(alicePrivateKey);
+		rsa.update(messageHash);
+		signature = rsa.sign();
 	}
-
+	
+	private static void encryptMessage() throws NoSuchAlgorithmException {
+		// WYLO: Combine the hash, signature, and message so they can be encrypted with a 3DES symmetric key
+		
+		
+		vout("Generating a 3DES symmetric key");
+		
+		KeyGenerator keyGenerator = KeyGenerator.getInstance("DESede");
+		SecretKey tripleDesKey = keyGenerator.generateKey();
+		symmetricKey = tripleDesKey.getEncoded();
+		
+		
+	}
+	
+	private static void vout(String text) {
+		if (verbose)
+			print(text);
+	}
+	
 	private static void help() {
 		print("\nCipherTalk v0.1 by Rob Johansen\n\n"
-				+ "Usage: java CipherTalk -a <address> -m <message> [-v]\n\n"
+				+ "Usage: java CipherTalk -a <address> -p <port> -m <message> [-v]\n\n"
 				+ "Arguments:\n"
 				+ "    -a <address>  The IP address of Bob's computer.\n"
+				+ "    -p <port>     The port number on which Bob's computer is listening.\n"
 				+ "    -m <message>  The message to sign, encrypt, and send to Bob.\n"
 				+ "    -v            Display verbose output.\n"
 				+ "    -h            Print this message and exit.\n");
 		System.exit(0);
 	}
 	
-	private static void error(String message) {
-		print("\n"+message+"\n");
+	private static void error(String text) {
+		print("\n"+text+"\n");
 		System.exit(1);
 	}
 	
-	private static void print(String message) {
-		System.out.println(message);
+	private static void print(String text) {
+		System.out.println(text);
 	}
 	
 }
