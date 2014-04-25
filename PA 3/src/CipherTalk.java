90dff77d47eb6f62711128b6cc89925f8e74b7fd
Rob Johansen <robert_johans22@hotmail.com>
2014-04-24 23:55:43 -0600

    Final WYLO and good night.

--- PA 3/src/CipherTalk.java
+++ PA 3/src/CipherTalk.java
@@ -138,7 +138,7 @@ public class CipherTalk {
 		//		 See this for int to 4 bytes: http://stackoverflow.com/questions/1936857/convert-integer-into-byte-array-java
 		//		 And this for 4 bytes to int: http://stackoverflow.com/questions/5616052/how-can-i-convert-a-4-byte-array-to-an-integer
 		//
-		//		 ...and remember that your Mac, Windows, and CADE boxes are all little-endian (so use .order(ByteOrder.LITTLE_ENDIAN) ? )
+		//		 ...and just do it in big-endian (i.e. don't worry about the order() method...)
 	}
 	
 	private static PublicKey loadPublicKey(String filename) throws FileNotFoundException,
